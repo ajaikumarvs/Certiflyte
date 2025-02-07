11769298ba387f1c988bfc720b1111f3013495d6
ajaikumarvs <ajaikumarvs@gmail.com>
2025-02-07 12:07:28 +0530

    updates to Splash animation

---
+++ SplashScreen.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Certiflyte
+{
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            double targetWidth = this.ActualWidth - 40; // Adjusted for margin
+
+            DoubleAnimation progressAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = targetWidth,
+                Duration = TimeSpan.FromSeconds(3),
+                FillBehavior = FillBehavior.HoldEnd
+            };
+
+            LoadingBarFill.BeginAnimation(WidthProperty, progressAnimation);
+            
+        }
+    }
+}
